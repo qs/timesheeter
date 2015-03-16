654bcfd002a60fcb8444d2d6d8d6e5a128b2352d
qs <mosdit@gmail.com>
2015-03-16 03:32:32 +0300

    Menu items refactoring.

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import appindicator
 import sys
 from datetime import datetime, timedelta
 
-
 STATUS_MESSAGE_LENGTH = 50
 MESSAGE_SPLIT_LENGTH = 40
 STATUS_UPDATE_FREQUENCY = 60 * 1000  # in milliseconds
 MESSAGE_ON_BREAK = 'You are on break'
+ON_BREAK = -1
 
 
 class TimeSheeter(object):
     def __init__(self):
         self.message = ''
         self.context_status = ''
-        self.since = datetime.now()
+        self.since = None
 
         self.indicator = appindicator.Indicator("timesheeter-iow",
                                                 "application-running",
@@ -22,37 +22,66 @@ class TimeSheeter(object):
         self.indicator.set_status(appindicator.STATUS_ACTIVE)
         self.menu = gtk.Menu()
         self.menu_items = {}
-        self.menu_setup()
-        self.set_message(MESSAGE_ON_BREAK)
 
-    def menu_setup(self):
-        self.update_context_status()
-        goto_ticket_item = gtk.MenuItem(self.context_status)
-        goto_ticket_item.connect("activate", self.action_goto_ticket)
-        settings_item = gtk.MenuItem("Settings")
-        settings_item.connect("activate", self.action_settings)
-        quit_item = gtk.MenuItem("Quit")
-        quit_item.connect("activate", self.action_quit)
-        set_ticket_item = gtk.MenuItem("Take a break")
-        set_ticket_item.connect("activate", self.action_quit)
+        self.menu_init_setup()
+        self.set_activity(ON_BREAK)
 
-        item_list = [
-            goto_ticket_item,
+
+    def menu_init_setup(self):
+        """
+        Initial context menu setup: set menu items.
+        """
+        self.menu_items['quit'] = gtk.MenuItem("Quit")
+        self.menu_items['ticket'] = gtk.MenuItem(self.context_status)
+        self.menu_items['settings'] = gtk.MenuItem("Settings")
+        self.menu_items['set_ticket'] = gtk.MenuItem("Set new aim")
+        self.menu_items['set_break'] = gtk.MenuItem("Take a break")
+        for name, item in self.menu_items.iteritems():
+            item.connect('activate', getattr(self, 'action_{0}'.format(name)))
+        menu_order = [
+            self.menu_items['ticket'],
             gtk.SeparatorMenuItem(),
-            set_ticket_item,
+            self.menu_items['set_ticket'],
+            self.menu_items['set_break'],
             gtk.SeparatorMenuItem(),
-            settings_item,
-            quit_item
+            self.menu_items['settings'],
+            self.menu_items['quit']
         ]
-
-        for item in item_list:
+        for item in menu_order:
             item.show()
             self.menu.append(item)
         self.indicator.set_menu(self.menu)
 
+    def set_activity(self, ticket_id, message=""):
+        """
+        Set new activity: update message and context message and since time.
+        If provided ticket is ON_BREAK, set on break mode: change menu 'ticket' behavior to
+        set new activity.
+        If previous activity was ON_BREAK, change menu 'ticket' behavior to 'to to ticket'.
+        If previous activity was another ticket, log time spent on that activity.
+        :param ticket_id: real ticket id from URL or ON_BREAK
+        :type ticket_id: int
+        """
+        self.since = datetime.now()
+        if ticket_id == ON_BREAK:
+            self.message = MESSAGE_ON_BREAK
+            self.menu_items['set_break'].hide()
+        else:
+            self.message = message
+            self.menu_items['set_break'].show()
+        self.update_context_status()
+        self.menu_items['ticket'].set_label(self.context_status)
+
     def update_context_status(self):
+        """
+        For current message update context menu item
+        to split it into multi-line and add since time.
+        set self.context_status
+        """
         message = ''
-        if self.message:
+        if self.message == MESSAGE_ON_BREAK:
+            message = MESSAGE_ON_BREAK
+        else:
             line_len = 0
             words = self.message.split(" ")
             for word in words:
@@ -61,12 +90,11 @@ class TimeSheeter(object):
                     line_len = 0
                 message += word + " "
                 line_len += len(word)
-        else:
-            message = MESSAGE_ON_BREAK
         delta = datetime.now() - self.since
         delta = '{0}h {1}m'.format(*str(delta).split(':')[:2])
         message += "\n  Since: {0}, {1}".format(self.since.strftime("%H:%M"), delta)
         self.context_status = message
+        self.set_message(self.message)
 
     def set_message(self, message):
         self.message = message
@@ -74,20 +102,38 @@ class TimeSheeter(object):
             else message[:STATUS_MESSAGE_LENGTH] + ".."
         self.indicator.set_label(message)
 
-    def action_goto_ticket(self, widget):
-        pass
-
-    def action_set_task(self, widget):
-        self.set_message("#111111: Investigating with something really strange and dangerous.")
-
     def action_quit(self, widget):
         sys.exit(0)
 
     def action_settings(self, widget):
+        """
+        Show settings window.
+        """
         pass
+
+    def action_ticket(self, widget):
+        """
+        Open in browser ticket url if it is not ON_BREAK.
+        """
+        pass
+
+    def action_set_ticket(self, widget):
+        """
+        Show window for setting ticket URL and aim message
+        """
+        message = "#111111 Investigating with something strange and dungerous"
+        ticket_id = 111111
+        self.set_activity(ticket_id, message)
+
+    def action_set_break(self, widget):
+        """
+        Set break
+        """
+        self.set_activity(ON_BREAK)
 
     def run(self):
         gtk.main()
+
 
 if __name__ == "__main__":
     indicator = TimeSheeter()
